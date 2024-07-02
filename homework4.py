4fddab9e5944b8f43b5af6b8bf5b98941885a3c1
julietvl <julietgorbunova@gmail.com>
2024-07-02 22:44:54 +0300

    отправляю задание

---
+++ homework4.py
@@ -0,0 +1,11 @@
+a = 'Здравствуйте, напишите ваш любимый цвет '
+my_string = input(a)
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(a.replace(' ', ''))
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
+
+
