df524b9d960deb0e855efbfee3f4e29ee8b83885
Julia Gorbunova <45624396+julietvl@users.noreply.github.com>
2024-07-03 13:53:17 +0300

    homework7.py

--- homework7.py
+++ homework7.py
@@ -1,9 +1,5 @@
 grades = [[5, 3, 3, 5, 4, 5, 3], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-students = {'Johnny',
-            'Bilbo',
-            'Steve',
-            'Khendrik',
-            'Aaron'}
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
 list_students = list(students)
 list.sort(list_students)
